26101a6ee74c8911b9f94231670e4ac599d7c382
Ioan Mihali <m0rgulash@gmail.com>
2018-03-08 19:33:34 +0200

    Update code for Heroku.

--- core/config/config.dev.js
+++ core/config/config.dev.js
@@ -4,9 +4,9 @@ let config = {};
 
 config.logFileDir = path.join(__dirname, '../../log');
 config.logFileName = 'app.log';
-config.dbHost = process.env.dbHost || 'localhost';
-config.dbPort = process.env.dbPort || '27017';
-config.dbName = process.env.dbName || 'trains';
-config.serverPort = process.env.serverPort || 3000;
+// config.dbHost = process.env.dbHost || 'localhost';
+// config.dbPort = process.env.dbPort || '27017';
+// config.dbName = process.env.dbName || 'trains';
+config.serverPort = process.env.PORT || 3000;
 
 export default config;
